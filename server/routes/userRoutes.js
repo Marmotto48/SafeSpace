21f0ab61e5436ae74e4f0bb9a6b5f20553a7b50e
donia <donia.bromdhane@gmail.com>
2021-11-29 21:42:54 +0100

    edit 6

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,46 +1,49 @@
-const express = require ('express');
+const express = require("express");
 const router = express.Router();
-const UserController = require("../controllers/UserControllers");
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const multer = require('multer');
-const cloudinary = require('../config/cloudinaryConfig');
+const {
+  getUsers,
+  getDoc,
+  getUser,
+  register,
+  login,
+  updateProfile,
+  deleteUser,
+  updateAvatar,
+} = require("../controllers/userControllers");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
+const cloudinary = require("../config/cloudinaryConfig");
 const postAuth = require("../middleware/postAuth");
-
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: 'DEV',
+    folder: "DEV",
   },
 });
 
 const upload = multer({ storage });
 
-
 const {
-    validation,
-    registerValidate,
-    loginValidate,
-  } = require("../middleware/validator");
+  validation,
+  registerValidate,
+  loginValidate,
+} = require("../middleware/validator");
 
-
-  //get Users
-router.get('/', UserController.getUsers)
+//get Users
+router.get("/", getUsers);
 //get Doctors
-router.get('/doctors/', UserController.getDoc)
+router.get("/doctors/", getDoc);
 //get User
-router.get('/:id', UserController.getUser)
+router.get("/:id", getUser);
 //Register User
-router.post("/register",registerValidate(), validation, UserController.register);
+router.post("/register", registerValidate(), validation, register);
 //Login User
-router.post("/login", validation, loginValidate(), UserController.login);
-router.put('/update/:id', UserController.updateProfile);
+router.post("/login", validation, loginValidate(), login);
+router.put("/update/:id", updateProfile);
 //delete user
-router.delete("/deleteuser/:id", UserController.deleteUser);
+router.delete("/deleteuser/:id", deleteUser);
 //update avatar
-router.put("/uploadimg/:id", upload.single('userImg'), postAuth, UserController.updateAvatar);
-
-
-
+router.put("/uploadimg/:id", upload.single("userImg"), postAuth, updateAvatar);
 
 module.exports = router;
